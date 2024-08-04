c42c0f9e5ffff8cbc37c6d53ab45fa892ba76a7d
WindyDay7 <279408285wf@gmail.com>
2024-08-04 21:58:24 +0800

    ew

---
+++ AVL_Tree/AVL_Tree.cpp
@@ -0,0 +1,15 @@
+#include "AVL_Tree.hpp"
+
+template<class T>
+bool AVLTree<T>::empty() const {
+    return root == nullptr;
+}
+
+template<class T>
+int AVLTree<T>::find(T value) const {
+    return 0;
+}
+
+
+
+
